7e01923254a98d797899f5a20a92a6b7cf367830
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-08-26 19:29:22 +0100

    error handling for no-controller scenario

--- lib/game/controller.rb
+++ lib/game/controller.rb
@@ -8,7 +8,7 @@ module Game
 
   class Controller
 
-    BUZZ_BYTES = [
+    BUZZ_BITS = [
       [2, 0],
       [2, 5],
       [3, 2],
@@ -19,16 +19,17 @@ module Game
       @window = window
       @usb_context = LIBUSB::Context.new
       @device = @usb_context.devices(
-        idVendor: 1356, idProduct: 2
+        idVendor: 0x054c, idProduct: 0x0002
       ).first
       reset_device_access
-      @handle = @device.open
+
+      @handle = @device.open if @device
     end
 
     def raw_data
       data = @handle.interrupt_transfer(
         :endpoint => 0x81,
-        :dataIn => 0x0008,
+        :dataIn => 0x0005,
         :timeout => 10
       )
       data.bytes.to_a
@@ -36,20 +37,24 @@ module Game
 
     def reset_device_access
       device = @usb_context.devices(
-        idVendor: 1356, idProduct: 2
+        idVendor: 0x054c, idProduct: 0x0002
       ).first
-      handle = device.open
-      handle.detach_kernel_driver(0)
-      handle.close
+      if @device
+        handle = device.open
+        handle.detach_kernel_driver(0)
+        handle.close
+      else
+        puts "USB device not found"
+      end
     rescue => er
+      puts "ignore this error"
       puts er.message
-      # nothing needs doing here
     end
 
 
     def update
       data = raw_data
-      BUZZ_BYTES.each_with_index do |lookup, i|
+      BUZZ_BITS.each_with_index do |lookup, i|
         if data[lookup[0]][lookup[1]] == 1
           puts "buzzer #{i + 1} pushed"
           @window.panellists[i].buzz
