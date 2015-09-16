530bc4bec0ee982172be2171d00b6502a370adae
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-09-16 20:08:25 +0100

    moved reporters out of game folder (unnecessary level)

---
+++ lib/reporters/command_line_reporter.rb
@@ -0,0 +1,60 @@
+module Reporters
+  class CommandLineReporter
+
+    # config:
+    #   width: Total terminal width
+    #   player_width: the size of the box around each panellist
+    def initialize(window, config={})
+      @window = window
+      @width = config['width'] || 100
+      @player_width = config['player_width'] || 25
+      @panellists = @window.panellists
+    end
+
+    def report
+      system 'clear'
+      spacer
+      puts current_subject.center(@width)
+      puts display_timer.center(@width)
+      spacer
+      panellists
+      spacer
+      puts "Next: #{next_subject}"
+    end
+
+    def spacer
+      2.times { puts '' }
+    end
+
+    def panellists
+      show_panellists_attr(nil)
+      show_panellists_attr(:name1)
+      show_panellists_attr(:name2)
+      show_panellists_attr(nil)
+      show_panellists_attr(:score)
+      show_panellists_attr(nil)
+    end
+
+    def show_panellists_attr(attr=nil)
+      @panellists.each do |panellist|
+        data = attr ? panellist.send(attr) : ''
+        pad_char = panellist.challenger ? '#' : ' '
+        print data.to_s.center(@player_width, pad_char)
+      end
+      puts ''
+    end
+
+    def display_timer
+      @window.timer.remaining.to_s
+    end
+
+    def current_subject
+      @window.subjects.current
+    end
+
+    def next_subject
+      @window.subjects.preview_next
+    end
+
+  end
+end
